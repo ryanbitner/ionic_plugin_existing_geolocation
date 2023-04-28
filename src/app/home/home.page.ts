131a266877de150e7359919f79c2fc6c4d17c4c0
ryanbitner <131702725+ryanbitner@users.noreply.github.com>
2023-04-28 13:46:52 -0600

    added name plugin
    added name plugin

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
+import { FullName } from 'fullname';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   currentPositionLat: string = '';
   currentPositionLong: string = '';
 
+  firstName: string = 'Ryan';
+  lastName: string = 'Bitner';
+
+  fullName: string | undefined;
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.getFullName();
+  }
 
    async getMyLocation() {
     let coords = await Geolocation.getCurrentPosition();
     this.currentPositionLat = coords.coords.latitude.toString();
     this.currentPositionLong = coords.coords.longitude.toString();
+  }
+
+ async getFullName() {
+    this.fullName = await FullName.getFullName({firstName: this.firstName, lastName: this.lastName});
   }
 
 }
